f530e5fad9fb469e86a3ee0a00ea7497c27f63b8
jewon119 <jewon119@gmail.com>
2022-12-05 14:35:51 +0900

    Add: sendFile로 main.html 랜더하기

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+//   res.send('Hello World!')
+    res.sendFile(__dirname + "/public/main.html")
 })
 
 app.listen(port, () => {
