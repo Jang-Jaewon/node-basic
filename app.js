bd1d693269ba1d4eb3bd1b9fbfd006fa5d581ade
jewon119 <jewon119@gmail.com>
2022-12-05 15:11:28 +0900

    Add: body-parser 추가 및 form 데이터 다루기

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.listen(port, () => {
+    console.log(`start!! express server on port ${port}`)
+})
+
 app.get('/', (req, res) => {
-//   res.send('Hello World!')
+    //   res.send('Hello World!')
     res.sendFile(__dirname + "/public/main.html")
 })
 
-app.use(express.static('public'))
-
-app.listen(port, () => {
-  console.log(`start!! express server on port ${port}`)
+app.post('/email_post', (req, res) => {
+    // get : req.param('email')
+    // post : req.body('email)
+    console.log(req.body.email)
+    res.send("<h1>welcome! " + req.body.email + "</h1>")
 })
