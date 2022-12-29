1d2e724a22ab6a26aa8c45c03a5f336376e79ef5
jewon119 <jewon119@gmail.com>
2022-12-29 15:11:11 +0900

    Add: 회원가입 Swagger docs 적용

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,5 +3,52 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/register", usersController.register);
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *      description: Used to register user
+ *      tags:
+ *          - users
+ *      parameters:
+ *          - in: body
+ *            name: User
+ *            description: User data
+ *            schema:
+ *              type: object
+ *              required:
+ *                 - firstName
+ *                 - lastName
+ *                 - emailId
+ *                 - password
+ *              properties:
+ *                  firstName:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 45
+ *                      example: Jaewon
+ *                  lastName:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 45
+ *                      example: Jang
+ *                  emailId:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 100
+ *                      example: jewon119@google.com
+ *                  password:
+ *                      type: string
+ *                      minLength: 1
+ *                      maxLength: 45
+ *                      example: 1q2w3e4r!@
+ *      responses:
+ *          '200':
+ *              description: Resource added successfully
+ *          '500':
+ *              description: Internal server error
+ *          '400':
+ *              description: Bad request
+ */
 
 module.exports = router;
